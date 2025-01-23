8d1a61920514a939dbd674ccb418dbc079bc34f2
Laurent Tempier <laurent.tempier@gmail.com>
2025-01-23 14:16:49 +0100

    from deploy.sh

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 
-window.onload = function () {
-
-};
+function copyToClipboard() {
+   var text = document.getElementById("text-to-copy").innerText;
+   navigator.clipboard.writeText(text).then(function () {
+      // alert("Texte copié dans le presse-papiers!");
+   }).catch(function (error) {
+      alert("Échec de la copie : " + error);
+   });
+}
